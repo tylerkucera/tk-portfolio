eb0b54050e6d834d84d53a70227dcd772c605f64
Tyler Kucera <tkucera10@gmail.com>
2018-04-18 22:17:30 -0400

    got rid of header color filter and added a few sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,53 +1,49 @@
 function nameSize() {
-  var mass = Math.max(26, 70-0.1*$(this).scrollTop()) + 'px';
-  $('#expandable').css({'font-size': mass, 'line-height': mass});
+	var mass = Math.max(26, 90-0.14*$(this).scrollTop()) + 'px';
+	$('#expandable').css({'font-size': mass, 'line-height': mass});
 }
 
 function menuPosition() {
-  var menuPositionX = Math.min(0, 0.47*$(this).scrollTop() - 210) + 'px';
-  $('#side-menu').css({'left': menuPositionX});
+	var menuPositionX = Math.min(0, 0.47*$(this).scrollTop() - 210) + 'px';
+	$('#side-menu').css({'left': menuPositionX});
 }
 
-function addNameClass() {
-  if (window.scrollY >= $('#about').offset().top) {
-    $('#expandable').addClass('white');
-  } else {
-    $('#expandable').removeClass('white');
-  }
+function smoothScroll(event) {
+	var target = $('#' + event.target.dataset.link);
+		var topPadding = 150;
+		
+		if (target) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top - topPadding
+			}, 700, function() {
+				var $target = $(target);
+				$target.focus();
+				if ($target.is(":focus")) {
+					return false;
+				} else {
+					$target.attr('tabindex','-1');
+					$target.focus();
+				};
+			});
+		}
 }
 
 $(function() {
-  nameSize();
-  menuPosition();
-  addNameClass();
+	nameSize();
+	menuPosition();
 
-  $(window).scroll(function() {
-    nameSize();
-    menuPosition();
-    addNameClass();
-  });
+	$(window).scroll(function() {
+		nameSize();
+		menuPosition();
+	});
 
-  $('.smooth-scroll').click(function() {
-    var link = $(this).data('link');
-    var target = $('#' + link);
-    
-    if (target) {
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 500, function() {
-        // Callback after animation
-        // Must change focus!
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
-        };
-      });
-    }
-  });
-  
+	$('.smooth-scroll').click(function() {
+		smoothScroll(event);
+	});
+
+	$('.scroll-button-container i').click(function(event){
+		smoothScroll(event);
+	});
+	
 });
